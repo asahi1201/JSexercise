19e4cd271228a1a77b2eb4b5335b22d3cd32fb41
hexu <hexu@sensoro.com>
2018-10-26 10:30:53 +0800

    reformat

--- client.js
+++ client.js
@@ -3,115 +3,109 @@ const readline = require('readline');
 const socket = io('http://localhost:6789');
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output : process.stdout
+  input: process.stdin,
+  output: process.stdout
 });
 
 let loginUsername = false;
-let userList = new Set();
+const userList = new Set();
 
-const consoleInput = prompt => new Promise(
-    resolve => rl.question(prompt,answer =>{resolve(answer)}));
+const consoleInput = (prompt) => new Promise((resolve) => rl.question(prompt, (answer) => { resolve(answer); }));
 
 
-socket.on('userOnline',user=>{
-    console.log('用户'+user+'已上线');
-    userList.add(user);
+socket.on('userOnline', (user) => {
+  console.log(`用户${user}已上线`);
+  userList.add(user);
 });
-socket.on('userOffline',user=>{
-    console.log('用户'+user+'已下线');
-    userList.delete(user);
+socket.on('userOffline', (user) => {
+  console.log(`用户${user}已下线`);
+  userList.delete(user);
 });
-socket.on('userList',ul=>{
-    for(let u of ul){
-        if(u!=loginUsername){
-            userList.add(u);
-        }
+socket.on('userList', (ul) => {
+  for (const u of ul) {
+    if (u!==loginUsername) {
+      userList.add(u);
     }
+  }
 });
-socket.on('chat',msg=>{
-    let msgJson = JSON.parse(msg);
-    console.log(msgJson.username+' @ '+new Date(msgJson.time).toLocaleString()+' 发来: '+msgJson.content);
+socket.on('chat', (msg) => {
+  const msgJson = JSON.parse(msg);
+  console.log(`${msgJson.username} @ ${new Date(msgJson.time).toLocaleString()} 发来: ${msgJson.content}`);
 });
-socket.on('heartbeat',()=>{
-    socket.emit('heartbeat',loginUsername);
+socket.on('heartbeat', () => {
+  socket.emit('heartbeat', loginUsername);
 });
 
-
-async function loginReq(){
-    let username = await consoleInput('请输入用户名：');
-    let password = await consoleInput('请输入密码：');
-    socket.emit('login',JSON.stringify({
-        username : username,
-        password : password
-    }));
-    await new Promise((done)=> {
-        socket.once('loginResult',(msg)=>{
-            console.log(JSON.parse(msg).msg);
-            loginUsername = JSON.parse(msg).username;
-            done();
-        });
-    });
+const waitForReg = (done) => {
+  socket.once('loginResult', (msg) => {
+    console.log(JSON.parse(msg).msg);
+    loginUsername = JSON.parse(msg).username;
+    done();
+  });
+};
+const waitForLogin = (done) => {
+  socket.once('regResult', (msg) => {
+    console.log(JSON.parse(msg).msg);
+    loginUsername = JSON.parse(msg).username;
+    done();
+  });
 };
 
-async function regReq(){
-    let username = await consoleInput('请输入用户名：');
-    let password = await consoleInput('请输入密码：');
-    socket.emit('reg',JSON.stringify({
-        username : username,
-        password : password
-    }));
-    await new Promise((done)=> {
-        socket.once('regResult',(msg)=>{
-            console.log(JSON.parse(msg).msg);
-            loginUsername = JSON.parse(msg).username;
-            done();
-        });
-    });
-};
-
-(async function(){
-    while(true){
-        if(loginUsername){
-            let opt1 = await consoleInput('请输入要进行聊天的用户ID（输入 \'999\'手动获取在线用户）\n');
-            switch(opt1){
-                case('999'):
-                    if(userList.size){
-                        console.log('目前的在线用户有:')
-                        for(let u of userList.keys()){
-                            console.log(u);
-                        }
-                    }else{
-                        console.log('目前没有其他在线用户.')
-                    }
-                    break;
-                default:
-                    if(userList.has(opt1)){
-                        let content = await consoleInput('请输入聊天的内容:\n');
-                        socket.emit('chat',JSON.stringify({
-                            from : loginUsername,
-                            to : opt1,
-                            content : content,
-                            time : new Date()
-                        }));
-                    }else{
-                        console.log('不存在该在线用户');
-                    }
-                    break;
-            }
-        }else{
-            let opt2 = await consoleInput('请输入选项：1.登陆 2.注册：\n');
-            switch(opt2){
-                case('1'):
-                    await loginReq();
-                    break;
-                case('2'):
-                    await regReq();
-                    break;
-            }
+(async function() {
+  let username = '';
+  let password = '';
+  while (true) {
+    if (loginUsername) {
+      const opt1 = await consoleInput('请输入要进行聊天的用户ID（输入 \'999\'手动获取在线用户）\n');
+      switch (opt1) {
+      case ('999'):
+        if (userList.size) {
+          console.log('目前的在线用户有:');
+          for (const u of userList.keys()) { console.log(u); }
+        } else {
+          console.log('目前没有其他在线用户.');
         }
-        
+        break;
+      default:
+        if (userList.has(opt1)) {
+          const content = await consoleInput('请输入聊天的内容:\n');
+          socket.emit('chat', JSON.stringify({
+            from: loginUsername,
+            to: opt1,
+            content,
+            time: new Date()
+          }));
+        } else {
+          console.log('不存在该在线用户');
+        }
+        break;
+      }
+    } else {
+      const opt2 = await consoleInput('请输入选项：1.登陆 2.注册：\n');
+      switch (opt2) {
+      case ('1'):
+        username = await consoleInput('请输入用户名：');
+        password = await consoleInput('请输入密码：');
+        socket.emit('login', JSON.stringify({
+          username,
+          password
+        }));
+        await new Promise(waitForLogin);
+        break;
+      case ('2'):
+        username = await consoleInput('请输入用户名：');
+        password = await consoleInput('请输入密码：');
+        socket.emit('reg', JSON.stringify({
+          username,
+          password
+        }));
+        await new Promise(waitForReg);
+        break;
+      default: break;
+      }
     }
-})();
+
+  }
+}());
 
 
--- server.js
+++ server.js
@@ -12,95 +12,96 @@ server.listen(6789);
 console.log('服务器在6789端口监听.');
 
 
-//连接
-io.on('connection',socket=>{
-    console.log('a user connect.')
-    setInterval(()=>{
-        for(let u in sockets){
-            sockets[u].emit('heartbeat');
-            heartbeatAck[u] = false;
-        }
-    },5000);//对每个客户端，每5秒发送一次心跳请求
+// 连接
+io.on('connection', (socket) => {
+  console.log('a user connect.');
+  // 对每个客户端，每5秒发送一次心跳请求
+  setInterval(() => {
+    for (const u in sockets) {
+      sockets[u].emit('heartbeat');
+      heartbeatAck[u] = false;
+    }
+  }, 5000);
+  // 对每个客户端，每13秒确认一次心跳
+  setInterval(() => {
+    for (const u in heartbeatAck) {
+      if (!heartbeatAck[u]) {
+        sockets[u].disconnect();
+        delete sockets.u;
+        delete heartbeatAck.u;
+        // 用户下线广播
+        socket.broadcast.emit('userOffline', u);
+      }
+    }
+  }, 13000);
 
-    setInterval(()=>{
-        for(let u in heartbeatAck){
-            if(!heartbeatAck[u]){
-                sockets[u].disconnect();
-                delete sockets.u;
-                delete heartbeatAck.u;
-                //用户下线广播
-                socket.broadcast.emit('userOffline',u);
-            }
-        }
-    },13000);//对每个客户端，每13秒确认一次心跳
-
-    socket.on('heartbeat',username=>{
-        heartbeatAck[username] = true;
-    });
+  socket.on('heartbeat', (username) => {
+    heartbeatAck[username] = true;
+  });
 
 
 
-    //登陆
-    socket.on('login',loginMsg=>{
-        chatdb.auth(loginMsg,loginResult=>{
-            if(loginResult.username){
-                console.log('loginResult:'+JSON.stringify(loginResult));
-                new Promise((resolve,reject)=>{
-                    socket.emit('loginResult',JSON.stringify({
-                        username:loginResult.username,
-                        msg:'登陆成功'
-                    }));
-                    sockets[loginResult['username']] = socket;
-                    //用户上线广播
-                    socket.broadcast.emit('userOnline',loginResult['username']);
-                    heartbeatAck[loginResult['username']] = true;
-                    resolve();
-                }).then(()=>{
-                    //发送在线用户列表
-                    socket.emit('userList',Object.keys(sockets));
-                });
-
-            }else{
-                socket.emit('loginResult',JSON.stringify({
-                    //仅当登陆成功时具有username属性
-                    msg:'用户不存在或密码错误'
-                }));
-            }
-        })
-    });
-
-    //注册
-    socket.on('reg',regMsg=>{
-        console.log('regMsg:'+regMsg);
-        chatdb.reg(regMsg,(regResult)=>{
-            socket.emit('regResult',JSON.stringify(regResult));
-            if(regResult.result){
-                sockets[regResult['username']] = socket;
-                socket.broadcast.emit('userOnline',regResult['username']);
-                socket.emit('userList',Object.keys(sockets));
-            }
+  // 登陆
+  socket.on('login', (loginMsg) => {
+    chatdb.auth(loginMsg, (loginResult) => {
+      if (loginResult.username) {
+        console.log(`loginResult:${JSON.stringify(loginResult)}`);
+        new Promise((resolve) => {
+          socket.emit('loginResult', JSON.stringify({
+            username: loginResult.username,
+            msg: '登陆成功'
+          }));
+          sockets[loginResult.username] = socket;
+          // 用户上线广播
+          socket.broadcast.emit('userOnline', loginResult.username);
+          heartbeatAck[loginResult.username] = true;
+          resolve();
+        }).then(() => {
+          // 发送在线用户列表
+          socket.emit('userList', Object.keys(sockets));
         });
-    });
 
-    socket.on('chat',chatMsg=>{
-        const chatJson = JSON.parse(chatMsg);
-        console.log('聊天信息：'+JSON.stringify(chatJson));
-        //私聊
-        sockets[chatJson['to']].emit('chat',JSON.stringify({
-            username : chatJson['from'],
-            content : chatJson['content'],
-            time : chatJson['time']
+      } else {
+        socket.emit('loginResult', JSON.stringify({
+          // 仅当登陆成功时具有username属性
+          msg: '用户不存在或密码错误'
         }));
-        //群聊
-        /*
-         */
+      }
     });
+  });
 
-    
+  // 注册
+  socket.on('reg', (regMsg) => {
+    console.log(`regMsg:${regMsg}`);
+    chatdb.reg(regMsg, (regResult) => {
+      socket.emit('regResult', JSON.stringify(regResult));
+      if (regResult.result) {
+        sockets[regResult.username] = socket;
+        socket.broadcast.emit('userOnline', regResult.username);
+        socket.emit('userList', Object.keys(sockets));
+      }
+    });
+  });
 
-    socket.on('disconnetion',logoutMsg=>{
-        const logoutJson = JSON.parse(logoutMsg);
-        delete sockets[logoutJson['username']];
-        socket.broadcast.emit('userOffline',logoutJson[username]);
-    })
+  socket.on('chat', (chatMsg) => {
+    const chatJson = JSON.parse(chatMsg);
+    console.log(`聊天信息：${JSON.stringify(chatJson)}`);
+    // 私聊
+    sockets[chatJson.to].emit('chat', JSON.stringify({
+      username: chatJson.from,
+      content: chatJson.content,
+      time: chatJson.time
+    }));
+    // 群聊
+    /*
+         */
+  });
+
+
+
+  socket.on('disconnetion', (logoutMsg) => {
+    const logoutJson = JSON.parse(logoutMsg);
+    delete sockets[logoutJson.username];
+    socket.broadcast.emit('userOffline', logoutJson.username);
+  });
 });
